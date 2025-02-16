416b58953b7e26b4a49fb6b73e91bd35f2984149
Pranav Gupta <146617022+Pranav-7302@users.noreply.github.com>
2025-02-16 11:34:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+let div = document.body.querySelector('.output')
+div.style.textAlign  = 'center'
+let head = document.createElement('h1')
+head.innerText = "Verify Your Account";
+head.id = 'verification_heading'
+div.appendChild(head);
+
+let p = document.createElement('p')
+p.innerText = 'We emailed you the six digit code to acciojob@email.com Enter the code below to confirm your email adaress'
+p.id = 'verification_subtext'
+div.append(p)
+
+let cont = document.querySelector('.code-container')
+cont.style.textAlign = 'center'
+let inputs = document.querySelectorAll('.code');
+
+inputs.forEach((input, index) => {
+    // Move to next input on typing
+    input.addEventListener("input", () => {
+        if (input.value && index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+    });
+
+    // Move to previous input on Backspace
+    input.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace" && !input.value && index > 0) {
+            inputs[index - 1].focus();
+        }
+    });
+});
+
+
+
+
